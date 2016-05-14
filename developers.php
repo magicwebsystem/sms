20dcedbb8a7fd9748ea00520fa61579c78cace5e
Pushkar Agrawal <agrawalpushkar@users.noreply.github.com>
2016-05-13 21:16:23 -0700

    Add files via upload

---
+++ developers.php
@@ -0,0 +1,68 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/pushkar.jpg" class="img-circle">
+										<hr>
+										<p>Name: Pushkar Agrawal</p>
+										<p>Address: Ghaziabad</p>
+										<p>Email: Agrawalpushkar@gmail.com</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/arun.jpg" class="img-circle">
+								<hr>
+																				
+
+										<p>Address: Arun Kumar Mall</p>
+    										<p>Address: Ghaziabad</p>
+ 										<p>Email: arunkumarmall123@gmail.com</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/rajni.jpg" class="img-circle">
+								<hr>
+										<p>Name: Rajni Agrawal</p>
+										<p>Address: Ghaziabad</p>
+										<p>Email: rajniagrawal@gmail.com</p>
+										<p>Position: Project Manager</p>
+							</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/anju.jpg" class="img-circle">
+								<hr>
+										<p>Name: Anju Gautam</p>
+										<p>Address: Ghaziabad</p>
+										<p>Email: anjugautam9211@gmail.com@gmail.com</p>
+										<p>Position: Database developer</p>
+								</center>
+								</div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,8 @@
+<center>
+		<footer>
+		
+		<p> M-Learning Copyright 2015 | Developed by: Pushkar Agrawal</p>
+                        
+		</footer>
+</center>
+
